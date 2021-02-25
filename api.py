6b80c25fdd300e931496449a0597d212b01b533b
SympactDev <47295195+SympactDev@users.noreply.github.com>
2021-02-25 14:38:32 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,26 @@
+
+# API YESYESYESYESYESYESYESYESYESYESYESYESYESYESYESYESYES
+# just dont try to use my api dickhead
+
+apiacceskey = ""
+
+#customization
+disablenames = False
+#if disablenames == true then customize this
+customname = ""
+
+
+#update
+version = 1.0
+updatenotes = "Added Brave Support"
+
+
+
+
+### DEBUG #####
+#### THIS IS NOT READY YET ####
+supersecretpin = ""
+devmode = False
+debug = False
+kankersoon = ""
+enabledkanker = False
